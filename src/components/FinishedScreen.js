2e42887a6c062966d061812179b2573bb399e97f
Sevgin SERBEST <severinseverinov@hotmail.com>
2023-12-25 18:41:47 +0300

    react-quiz finished

---
+++ src/components/FinishedScreen.js
@@ -0,0 +1,25 @@
+function FinishedScreen({ points, allPoints, highScore, dispatch }) {
+  const percentage = (points / allPoints) * 100;
+  let emoji;
+  if (percentage === 100) emoji = "🏅";
+  if (percentage >= 80 && percentage < 100) emoji = "🥳";
+  if (percentage >= 50 && percentage < 80) emoji = "😊";
+  if (percentage > 0 && percentage < 50) emoji = "🤔";
+  if (percentage === 0) emoji = "🤦";
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span> You scored <strong>{points}</strong> out of{" "}
+        {allPoints}({Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(Highscore: {highScore} points)</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart quiz
+      </button>
+    </>
+  );
+}
+export default FinishedScreen;
---
+++ src/components/Question.js
@@ -0,0 +1,12 @@
+import Option from "./Option.js";
+
+function Question({ question, answer, dispatch }) {
+  return (
+    <div>
+      <h4>{question.question}</h4>
+      <Option question={question} dispatch={dispatch} answer={answer} />
+    </div>
+  );
+}
+
+export default Question;
